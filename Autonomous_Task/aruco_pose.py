d81da7a4012b28999dc2a832c63da55673427649
Nada Abbas <N.Elsayed@nu.edu.eg>
2023-05-14 00:34:02 +0200

    Autonomous_task_markerID_updated in both files

--- Autonomous_Task/aruco_pose.py
+++ Autonomous_Task/aruco_pose.py
@@ -5,27 +5,45 @@ import numpy as np
 import rospy
 from cv_bridge import CvBridge
 from geometry_msgs.msg import PoseStamped
-from std_msgs.msg import Header
-import argparse  # added import statement for argparse
+from std_msgs.msg import Header,Int32
 from sensor_msgs.msg import Image
+
+
 bridge = CvBridge()
 
-def image_callback(image_msg, args):
-    bridge, pose_pub, namespace = args
-    detect_aruco(image_msg, bridge, pose_pub, namespace)
+def image_callback(image_msg):
+    detect_aruco(image_msg, bridge, pose_pub)
 
-def detect_aruco(image_msg, bridge, pose_pub, args):
+def detect_aruco(image_msg, bridge, pose_pub):
+    # Convert ROS image message to OpenCV image
     cv_image = bridge.imgmsg_to_cv2(image_msg, desired_encoding='bgr8')
+
+    # Initialize dictionary and parameters
     dictionary = cv2.aruco.Dictionary_get(cv2.aruco.DICT_ARUCO_ORIGINAL)
     parameters = cv2.aruco.DetectorParameters_create()
+
+    # Camera matrix and distortion coefficients
     camera_matrix = np.array([[628.158, 0., 324.099], [0., 628.156, 260.908], [0., 0., 1.]])
     dist_coeffs = np.array([0.0995485, -0.206384, 0.00754589, 0.00336531, 0])
+
+    # Detect markers in image
     marker_corners, marker_ids, _ = cv2.aruco.detectMarkers(cv_image, dictionary, parameters=parameters)
 
-    if marker_ids is not None and args.marker_id in marker_ids:
-        idx = np.where(marker_ids == args.marker_id)[0][0]
-        rvecs, tvecs, _ = cv2.aruco.estimatePoseSingleMarkers(marker_corners, args.marker_length, camera_matrix, dist_coeffs)
-        marker_center = tvecs[idx][0] + np.array([0, 0, args.marker_length / 2.0])
+    if marker_ids is not None:
+        marker_length=3 #cm
+        max_id = max(marker_ids)
+        idx = np.where(marker_ids == max_id)[0][0]
+        marker_id = marker_ids[idx]
+        rospy.loginfo(f"Detected marker ID: {marker_id}")
+        marker_id_pub.publish(marker_id)
+
+        # Estimate pose of marker with specified ID
+        rvecs, tvecs, _ = cv2.aruco.estimatePoseSingleMarkers(marker_corners,marker_length, camera_matrix, dist_coeffs)
+
+        # Get position of center of marker in camera frame
+        marker_center = tvecs[idx][0] + np.array([0, 0, marker_length / 2.0])
+
+        # Publish pose as ROS message
         header = Header(stamp=image_msg.header.stamp, frame_id='camera_frame')
         pose = PoseStamped(header=header)
         pose.pose.position.x = marker_center[0]
@@ -36,6 +54,7 @@ def detect_aruco(image_msg, bridge, pose_pub, args):
         pose.pose.orientation.y = q[1, 0]
         pose.pose.orientation.z = q[2, 0]
         pose.pose.orientation.w = np.sqrt(1 + q[0, 0] + q[1, 1] + q[2, 2]) / 2.0
+        
         pose_pub.publish(pose)
         # Add x, y, z text
         font = cv2.FONT_HERSHEY_SIMPLEX
@@ -60,12 +79,14 @@ def detect_aruco(image_msg, bridge, pose_pub, args):
 if __name__ == '__main__':
     # Define ROS node name
     rospy.init_node('pose_estimator')
-    parser = argparse.ArgumentParser(description='Detect ArUco markers and estimate their poses')
-    parser.add_argument('--marker_id', type=int, default=0, help='ID of marker to detect')
-    parser.add_argument('--marker_length', type=float, default=0.1, help='Length of marker in meters')
-    args = parser.parse_args()
+    # Set up pose publisher
     pose_topic = '/marker_pose'
     pose_pub = rospy.Publisher(pose_topic, PoseStamped, queue_size=10)
+    # Set up marker ID publisher
+    marker_id_topic = '/marker_id'
+    marker_id_pub = rospy.Publisher(marker_id_topic, Int32, queue_size=10)
+
+    # Set up image subscriber
     image_topic = '/image_topic_2'
-    image_sub = rospy.Subscriber(image_topic, Image, image_callback, (bridge, pose_pub, args))
+    image_sub = rospy.Subscriber(image_topic, Image, image_callback)
     rospy.spin()
